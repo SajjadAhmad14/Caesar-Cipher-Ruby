7587c36640ea36e4dd86fafa1112d2273055e9d9
Sajjad Ahmad <jogimar14@gmail.com>
2020-09-17 15:29:58 +0500

    Add spec for caesar

--- spec/caesar_spec.rb
+++ spec/caesar_spec.rb
@@ -1,11 +1,15 @@
 require_relative '../caesar'
 describe Cipher do
+  let(:cipher) {Cipher.new("i", 1)}
   describe '#initialize(str, key)' do
     it "create instance of Cipher" do
-      cipher = Cipher.new("sajjad", 5)
+      cipher = Cipher.new("i", 1)
       expect(cipher).to be_a Cipher
     end
   end
   describe '#caesar' do
+    it "return a string after ciphering it using a key" do
+      expect(cipher.caesar).to eql("j")
+    end
   end
 end
