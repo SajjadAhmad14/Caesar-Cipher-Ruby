41b9b4125f12489ea363c9535828078bf3ab8e04
Sajjad Ahmad <jogimar14@gmail.com>
2020-09-17 18:14:18 +0500

    Fix rubocop errors

--- caesar.rb
+++ caesar.rb
@@ -1,27 +1,32 @@
+# frozen_string_literal: true
+
+# This class for Caesar Cipher
+
 class Cipher
   def initialize(str, key)
     @str = str
     @key = key
   end
+
   def caesar
-   ascii_array = @str.bytes
-   str1 = ""
-   ascii_array.size.times do |i|
-     if ascii_array[i] >= 65 && ascii_array[i] <= 90
-       str1 << (((ascii_array[i] - 65) + @key) % 26 + 65).chr
-     elsif ascii_array[i] >= 97 && ascii_array[i] <= 122
-       str1 << (((ascii_array[i] - 97) + @key) % 26 + 97).chr
-     else
-       str1 << ascii_array[i]
-     end
-   end
-   str1
+    ascii_array = @str.bytes
+    str1 = ''
+    ascii_array.size.times do |i|
+      str1 << if ascii_array[i] >= 65 && ascii_array[i] <= 90
+                (((ascii_array[i] - 65) + @key) % 26 + 65).chr
+              elsif ascii_array[i] >= 97 && ascii_array[i] <= 122
+                (((ascii_array[i] - 97) + @key) % 26 + 97).chr
+              else
+                ascii_array[i]
+              end
+    end
+    str1
   end
 end
 
-puts "Enter any text to cipher it!"
+puts 'Enter any text to cipher it!'
 text = gets.chomp
-puts "Enter key!"
+puts 'Enter key!'
 key = gets.chomp.to_i
 cipher = Cipher.new(text, key)
 p cipher.caesar
--- spec/caesar_spec.rb
+++ spec/caesar_spec.rb
@@ -1,27 +1,29 @@
+# frozen_string_literal: true
+
 require_relative '../caesar'
 describe Cipher do
   describe '#initialize(str, key)' do
-    it "create instance of Cipher" do
-      cipher = Cipher.new("i", 1)
+    it 'create instance of Cipher' do
+      cipher = Cipher.new('i', 1)
       expect(cipher).to be_a Cipher
     end
   end
   describe '#caesar' do
-    cipher = Cipher.new("i", 1)
-    it "return a string after ciphering it using a key" do
-      expect(cipher.caesar).to eql("j")
+    cipher = Cipher.new('i', 1)
+    it 'return a string after ciphering it using a key' do
+      expect(cipher.caesar).to eql('j')
     end
   end
   describe '#caesar' do
-    cipher = Cipher.new("z", 1)
-    it "wrap around z to a" do
-      expect(cipher.caesar).to eql("a")
+    cipher = Cipher.new('z', 1)
+    it 'wrap around z to a' do
+      expect(cipher.caesar).to eql('a')
     end
   end
   describe '#caesar' do
-    cipher = Cipher.new("Z", 1)
-    it "wrap around Z to A" do
-      expect(cipher.caesar).to eql("A")
+    cipher = Cipher.new('Z', 1)
+    it 'wrap around Z to A' do
+      expect(cipher.caesar).to eql('A')
     end
   end
 end
