855ea5aad8608b4b7bbb4eda1b0a86929a30d369
Sajjad Ahmad <jogimar14@gmail.com>
2020-09-17 17:26:00 +0500

    Add conditions to wrap around string from z to a and Z to A

--- caesar.rb
+++ caesar.rb
@@ -7,15 +7,17 @@ class Cipher
    ascii_array = @str.bytes
    str1 = ""
    ascii_array.size.times do |i|
-     if ascii_array[i] >= 65 && ascii_array[i] <= 122
-       str1 << (ascii_array[i] + @key).chr
+     if ascii_array[i] >= 65 && ascii_array[i] <= 90
+       str1 << (((ascii_array[i] - 65) + @key) % 26 + 65).chr
+     elsif ascii_array[i] >= 97 && ascii_array[i] <= 122
+       str1 << (((ascii_array[i] - 97) + @key) % 26 + 97).chr
      else
        str1 << ascii_array[i]
      end
-    end
+   end
    str1
   end
 end
 
-cipher = Cipher.new("I am Sajjad!", 4)
+cipher = Cipher.new("I am ZzyYxXwW!", 4)
 p cipher.caesar
