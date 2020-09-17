274195b84e6a894ac3b0213492b02fe915b5041f
Sajjad Ahmad <jogimar14@gmail.com>
2020-09-17 15:29:23 +0500

    #caesar return a string after add key to every character

--- caesar.rb
+++ caesar.rb
@@ -5,7 +5,17 @@ class Cipher
   end
   def caesar
    ascii_array = @str.bytes
+   str1 = ""
+   ascii_array.size.times do |i|
+     if ascii_array[i] >= 65 && ascii_array[i] <= 122
+       str1 << (ascii_array[i] + @key).chr
+     else
+       str1 << ascii_array[i]
+     end
+    end
+   str1
   end
 end
 
-cipher = Cipher.new("sajjad", 4)
+cipher = Cipher.new("I am Sajjad!", 4)
+p cipher.caesar
